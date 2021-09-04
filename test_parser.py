0f84092a0910ac63d1a727c1fe82da9dd4737a6c
fxmag <leshchuk@yahoo.com>
2021-09-04 20:27:15 +0300

    first commit

--- test_parser.py
+++ test_parser.py
@@ -4,5 +4,4 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    time.sleep(30)
     browser.find_element_by_css_selector("#login_link")
